be1d230f190936e4b18a83a9b018895f64bc1821
Gabriel Davison <mail@gabrieldavison.me.uk>
2022-02-17 14:38:19 +0000

    Add recipe page

---
+++ src/recipe/Recipe.js
@@ -0,0 +1,65 @@
+import React from "react";
+import data from "./data";
+
+const Recipe = () => {
+  return (
+    <div style={styles.recipe}>
+      <h1>So Fresh</h1>
+      <h2>{data.recipeTitle}</h2>
+      <div style={styles.recipeSummary}>
+        <div>
+          <img src={data.recipeImage}></img>
+        </div>
+        <div>
+          <div style={styles.recipeDescription}>
+            <p>{data.recipeDescription}</p>
+          </div>
+          <div style={styles.ingredientsAllergens}>
+            <section style={styles.listWrapper}>
+              <h3>Ingredients</h3>
+              <ul>
+                {data.ingredients.map((ingredient, i) => (
+                  <li key={i}>{ingredient}</li>
+                ))}
+              </ul>
+            </section>
+            <section style={styles.listWrapper}>
+              <h3>Allergens</h3>
+              <ul>
+                {data.allergens.map((allergen, i) => (
+                  <li key={i}>{allergen}</li>
+                ))}
+              </ul>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  recipe: {
+    maxWidth: "1000px",
+    margin: "auto",
+  },
+  recipeSummary: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  recipeDescription: {
+    display: "flex",
+    alignItems: "center",
+    padding: "1rem",
+  },
+  ingredientsAllergens: {
+    padding: "1rem",
+    display: "flex",
+    flexDirection: "row",
+  },
+  listWrapper: {
+    width: "50%",
+  },
+};
+
+export default Recipe;
---
+++ src/recipe/data.js
@@ -0,0 +1,15 @@
+const data = {
+  recipeTitle: "First Recipe",
+  recipeImage: "./img/recipe1Image.jpeg",
+  recipeDescription:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rhoncus dapibus tempus. Nam leo diam, tincidunt vel posuere ut, malesuada ac ligula. Curabitur at nisl sit amet libero gravida vehicula vitae vel mi. Sed posuere vehicula nisl id suscipit. Phasellus felis sem, commodo eu porta id, faucibus sit amet massa. In consectetur sem id ante iaculis, vel molestie felis scelerisque. Curabitur id turpis id orci ornare rutrum vel id lectus. Nunc tempor nec felis eget bibendum. Morbi fermentum ac nisi et ullamcorper. Nulla luctus leo sed tellus lacinia, id fermentum velit porta.",
+  ingredients: [
+    "ingredient1",
+    "ingredient2",
+    "ingredient3",
+    "ingredient4",
+    "ingredient5",
+  ],
+  allergens: ["Eggs", "Soy", "Peanuts"],
+};
+export default data;
