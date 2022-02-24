936014d944f510d29dffc14b11992e1fe28fe93b
Gabriel Davison <mail@gabrieldavison.me.uk>
2022-02-24 15:29:33 +0000

    add recipe route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import AccountPage from "./Account/AccountPage";
 import SubscriptionsPage from "./subscriptionsPage/SubscriptionsPage";
 import Subscription from "./subscription/Subscription";
+import Recipe from "./recipe/Recipe";
 import users from "./datamock.json";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
@@ -19,6 +20,7 @@ function App() {
           element={<AccountPage user={users[0]} />}
         ></Route>
         <Route path="/subscription" element={<Subscription />}></Route>
+        <Route path="/recipe" element={<Recipe />}></Route>
       </Routes>
     </BrowserRouter>
   );
