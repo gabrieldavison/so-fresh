87812902deba6e821f19c9e09be020c326d1b29e
Gabriel Davison <mail@gabrieldavison.me.uk>
2022-02-24 14:59:56 +0000

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import AccountPage from "./Account/AccountPage";
+import SubscriptionsPage from "./subscriptionsPage/SubscriptionsPage";
 import users from "./datamock.json";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <body>
-      <AccountPage user={users[0]} />
-    </body>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">All Subscriptions</Link>
+        <Link to="/account">Account</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<SubscriptionsPage />}></Route>
+        <Route
+          path="/account"
+          element={<AccountPage user={users[0]} />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
