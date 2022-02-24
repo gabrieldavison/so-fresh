456c47aaec7aa9c0619af11844c1b4a5f1851cfc
Gabriel Davison <mail@gabrieldavison.me.uk>
2022-02-24 15:28:28 +0000

    link to recipe

--- src/subscription/RecipeCard.js
+++ src/subscription/RecipeCard.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RecipeCard = ({ data }) => {
   console.log(data);
   return (
     <section style={styles.recipeCard}>
-      <h3>{data.title}</h3>
+      <h3>
+        <Link to="/recipe">{data.title}</Link>
+      </h3>
       <img style={styles.recipeImage} src={data.recipeImage}></img>
       <p>{data.recipeSummary}</p>
     </section>
