c069985b9802bab6839bb20c3ffb93c77a0634ae
Katie Reed <ekat@fb.com>
2022-02-24 14:13:29 +0000

    trying to fix styles of subscriptions page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import AccountPage from "./Account/AccountPage";
 import users from "./datamock.json";
+import SubscriptionsPage from "./subscriptionsPage/SubscriptionsPage";
+import Subscription from "./subscription/Subscription";
+import Recipe from "./recipe/Recipe";
 
 function App() {
   const user = users[3];
 
   return (
     <div className="App">
-      <AccountPage user={user} />
+      <SubscriptionsPage />
     </div>
   );
 }
--- src/subscriptionsPage/Button.js
+++ src/subscriptionsPage/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Button.css";
 
 const Button = ({ onClick }) => {
   return <div className="button" onClick={onClick} />;
