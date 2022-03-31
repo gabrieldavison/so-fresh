e60fcc300a082052443337f98341b2b67b2fe472
Gabriel Davison <mail@gabrieldavison.me.uk>
2022-03-31 15:21:53 +0100

    change width

--- src/recipe/Recipe.js
+++ src/recipe/Recipe.js
@@ -4,44 +4,46 @@ import Header from "../subscriptionsPage/Header";
 
 const Recipe = () => {
   return (
-    <div style={styles.recipe}>
+    <>
       <Header />
-      <h2 data-cy="recipe-title">{data.recipeTitle}</h2>
-      <div style={styles.recipeSummary}>
-        <div>
-          <img data-cy="recipe-image" src={data.recipeImage}></img>
-        </div>
-        <div>
-          <div style={styles.recipeDescription}>
-            <p data-cy="recipe-description">{data.recipeDescription}</p>
+      <div style={styles.recipe}>
+        <h2 data-cy="recipe-title">{data.recipeTitle}</h2>
+        <div style={styles.recipeSummary}>
+          <div>
+            <img data-cy="recipe-image" src={data.recipeImage}></img>
           </div>
-          <div style={styles.ingredientsAllergens}>
-            <section style={styles.listWrapper}>
-              <h3>Ingredients</h3>
-              <ul data-cy="ingredients-list">
-                {data.ingredients.map((ingredient, i) => (
-                  <li key={i}>{ingredient}</li>
-                ))}
-              </ul>
-            </section>
-            <section style={styles.listWrapper}>
-              <h3>Allergens</h3>
-              <ul data-cy="allergens-list">
-                {data.allergens.map((allergen, i) => (
-                  <li key={i}>{allergen}</li>
-                ))}
-              </ul>
-            </section>
+          <div>
+            <div style={styles.recipeDescription}>
+              <p data-cy="recipe-description">{data.recipeDescription}</p>
+            </div>
+            <div style={styles.ingredientsAllergens}>
+              <section style={styles.listWrapper}>
+                <h3>Ingredients</h3>
+                <ul data-cy="ingredients-list">
+                  {data.ingredients.map((ingredient, i) => (
+                    <li key={i}>{ingredient}</li>
+                  ))}
+                </ul>
+              </section>
+              <section style={styles.listWrapper}>
+                <h3>Allergens</h3>
+                <ul data-cy="allergens-list">
+                  {data.allergens.map((allergen, i) => (
+                    <li key={i}>{allergen}</li>
+                  ))}
+                </ul>
+              </section>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 const styles = {
   recipe: {
-    maxWidth: "1000px",
+    maxWidth: "1200px",
     margin: "auto",
   },
   recipeSummary: {
@@ -52,6 +54,7 @@ const styles = {
     display: "flex",
     alignItems: "center",
     padding: "1rem",
+    fontSize: "1.3rem"
   },
   ingredientsAllergens: {
     padding: "1rem",
